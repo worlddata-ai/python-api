a0127ddec2431fd8c2bed0f325bcfa35ee6141a6
Vikas Patel <vikas.patel15793@gmail.com>
2021-05-26 15:18:33 +0530

    Fixed search text keyword issue

--- tests/test_advance_search.py
+++ tests/test_advance_search.py
@@ -1,11 +1,11 @@
 def test_advance_search(world_data):
-    search_result = world_data.advance_search(seatch_text='india', sector='MACROECONOMICS, FINANCE', sub_sector='GLOBAL ECONOMIC MONITOR',
+    search_result = world_data.advance_search(search_text='india', sector='MACROECONOMICS, FINANCE', sub_sector='GLOBAL ECONOMIC MONITOR',
                                               super_region='GLOBAL DATA', source='WORLD BANK', size=10, offset=0)
     return search_result
 
 
 def test_advance_search_attributes(world_data):
-    attributes = world_data.advance_search_attributes(seatch_text='india', sector='MACROECONOMICS, FINANCE',
+    attributes = world_data.advance_search_attributes(search_text='india', sector='MACROECONOMICS, FINANCE',
                                                       sub_sector='GLOBAL ECONOMIC MONITOR',
                                                       super_region='GLOBAL DATA', source='WORLD BANK')
     return attributes
