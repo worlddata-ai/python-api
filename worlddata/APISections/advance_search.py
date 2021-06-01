bf46aaaf63273e862a45ac4cecc612733c32bd75
Vikas Patel <vikas.patel15793@gmail.com>
2021-06-01 23:58:00 +0530

    Remove attribute filters

--- worlddata/APISections/advance_search.py
+++ worlddata/APISections/advance_search.py
@@ -8,8 +8,8 @@ class WorldDataAdvanceSearch(WorldDataBase):
                                   super_region=super_region,
                                   source=source, size=size, offset=offset, kwargs=kwargs)
 
-    def advance_search_attributes(self, search_text, sector, sub_sector, super_region, source, attributes,
+    def advance_search_attributes(self, search_text, sector, sub_sector, super_region, source,
                                   **kwargs):
         return self.call_api_post("attributes", search_text=search_text, sector=sector, sub_sector=sub_sector,
                                   super_region=super_region,
-                                  source=source, attribute_filters=attributes, kwargs=kwargs)
+                                  source=source, kwargs=kwargs)
