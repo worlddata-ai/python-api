91511aa85916ae818ed6707a5fccea7bf8fc9228
Vikas Patel <vikas.patel15793@gmail.com>
2021-07-07 15:24:35 +0530

    Added API to do advanced search with bucket

--- worlddata/APISections/advance_search.py
+++ worlddata/APISections/advance_search.py
@@ -10,6 +10,9 @@ class WorldDataAdvanceSearch(WorldDataBase):
     def advance_search(self, search_text, sector, sub_sector, super_region, source, size, offset, **kwargs):
         return self.call_api_post("advanced-search", search_text=search_text, sector=sector, sub_sector=sub_sector,
                                   super_region=super_region, source=source, size=size, offset=offset, kwargs=kwargs)
+    
+    def advance_search_with_bucket(self, bucket, size, offset, **kwargs):
+        return self.call_api_post("advanced-search-with-bucket", bucket, size=size, offset=offset, kwargs=kwargs)
 
     def advance_search_attributes(self, search_text, sector, sub_sector, super_region, source, **kwargs):
         return self.call_api_post("attributes", search_text=search_text, sector=sector, sub_sector=sub_sector,
